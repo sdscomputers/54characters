3725be736a39b4b580df3aee77a6ed0489c8abd9
Ryan <rebelwebdevelopment@gmail.com>
2016-10-21 21:56:50 -0500

    added ethnicity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022015601) do
+ActiveRecord::Schema.define(version: 20161022024642) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "first_name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20161022015601) do
     t.boolean  "veteran"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "ethnicities", force: :cascade do |t|
+    t.string   "description"
+    t.boolean  "active"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
