384aad5e5f99af80774d7376bf631b58e16da679
Joey Meyer <joeymeyer1986@gmail.com>
2016-10-22 02:13:50 -0500

    added tinymce gem, styled organization index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022033920) do
+ActiveRecord::Schema.define(version: 20161022052002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20161022033920) do
     t.boolean  "active"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "organization_name"
+    t.string   "address"
+    t.string   "description"
+    t.string   "email"
+    t.string   "phone"
+    t.boolean  "active"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "users", force: :cascade do |t|
