662ec2458a6b0245d9f5129e0c7a42bb2f680d15
Ryan <rebelwebdevelopment@gmail.com>
2016-10-22 00:11:53 -0500

    added genders to importer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022033920) do
+ActiveRecord::Schema.define(version: 20161022045725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161022033920) do
     t.boolean  "veteran"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "gender"
   end
 
   create_table "ethnicities", force: :cascade do |t|
--- lib/hud_importer/lib/hud_importer/client_importer.rb
+++ lib/hud_importer/lib/hud_importer/client_importer.rb
@@ -11,9 +11,11 @@ module HudImporter
     def import
       csv_data.each_with_index do |row, i|
         next if i == 0
-        client = Client.create(first_name: row[1], middle_name: row[2], last_name: row[3],
-                                  name_quality: row[4], birthday: row[7], birthday_quality: row[8],
-                                  ss_number: row[5], veteran: row[17])
+        client = Client.create(first_name: row[1], middle_name: row[2],
+                               last_name: row[3], name_quality: row[4],
+                               birthday: row[7], birthday_quality: row[8],
+                               ss_number: row[5], veteran: row[17],
+                               gender: get_gender(row[15]))
         create_ethnicity_records(row, client.id)
       end
     end
@@ -27,6 +29,19 @@ module HudImporter
       ClientEthnicity.create(client_id: client_id, ethnicity_id: 4) if row[12] == "1"
       ClientEthnicity.create(client_id: client_id, ethnicity_id: 5) if row[13] == "1"
       ClientEthnicity.create(client_id: client_id, ethnicity_id: 6) if row[14] == "1"
+    end
+
+    def get_gender(gender)
+      case gender
+      when '1'
+        'M'
+      when '0'
+        'F'
+      when '3'
+        'T'
+      else
+        nil
+      end
     end
   end
 end
