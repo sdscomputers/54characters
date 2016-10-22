1cb58879e9685950fef45062261ccf899e88ece0
Joey Meyer <joeymeyer1986@gmail.com>
2016-10-22 00:32:40 -0500

    added fields to organization table migration

--- db/migrate/20161022052002_create_organizations.rb
+++ db/migrate/20161022052002_create_organizations.rb
@@ -1,7 +1,12 @@
 class CreateOrganizations < ActiveRecord::Migration[5.0]
   def change
     create_table :organizations do |t|
-
+      t.string :organization_name
+      t.string :address
+      t.string :description
+      t.string :email
+      t.string :phone
+      t.boolean :active
       t.timestamps
     end
   end
