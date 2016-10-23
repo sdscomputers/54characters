f98f8966045bcaca2de20eaea0ce602fd96efb63
Joey Meyer <joeymeyer1986@gmail.com>
2016-10-23 00:33:12 -0500

    linked top organizations with beds to front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @organizations = Organization.all
+    organizations = Organization.where("organizations.max_beds - organizations.client_bed_count > 0").sort { |x,y| x.open_beds <=> y.open_beds }
+    @organizations = organizations.first(6)
   end
 end
