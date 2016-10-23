59cf2b78f76249db4027972d1be78569bc40dd88
Ryan <rebelwebdevelopment@gmail.com>
2016-10-22 20:18:38 -0500

    added job to notify organizations

--- app/mailers/referral_mailer.rb
+++ app/mailers/referral_mailer.rb
@@ -7,10 +7,17 @@ class ReferralMailer < ApplicationMailer
   def referring_organization_notification(referred_id, referring_id)
     @referred = Organization.find(referred_id)
     @referring = Organization.find(referring_id)
+    mail(to: 'organizationb@gmail.com', subject: '54 Characters - Referral Sent')
   end
 
   def referred_organization_notification(referred_id, referring_id)
     @referred = Organization.find(referred_id)
     @referring = Organization.find(referring_id)
+    mail(to: 'organizationa@gmail.com', subject: '54 Characters - You have been Referred')
+  end
+
+  def no_response_notification(referral_id)
+    @referral = Referral.find(referral_id)
+    mail(to: 'organizationb@gmail.com', subject: '54 Characters - No Response')
   end
 end
